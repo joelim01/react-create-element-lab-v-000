a3ea1aae7e07eb5cad6d6a93f1ca3de10c690c6b
Joseph Lim <joseph.d.lim@gmail.com>
2018-07-26 17:05:38 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 const title = React.createElement('h1', {}, 'An Awesome Person');
 const description = React.createElement('p', {}, 'Who is learning React');
-const list = React.createElement('ul', {className: 'my-interests'}, '<li>Javascript</li><li>React</li><li>Movies</li><li>Ice Cream</li>')
+const list = React.createElement('ul', {className: 'my-interests'}, 
+  [
+    React.createElement('li', {}, 'Javascript'),
+    React.createElement('li', {}, 'React'),
+    React.createElement('li', {}, 'Movies'),
+    React.createElement('li', {}, 'Javascript')
+  ])
 const meInReact = React.createElement('div', {className: 'me'}, [title, description, list]);
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
