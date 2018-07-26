8fa8319d411d0e1a47ea9982bcb1d13a43c651ad
Joseph Lim <joseph.d.lim@gmail.com>
2018-07-26 17:04:07 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const description = React.createElement('p', {}, 'Who is learning React');
+const list = React.createElement('ul', {className: 'my-interests'}, '<li>Javascript</li><li>React</li><li>Movies</li><li>Ice Cream</li>')
+const meInReact = React.createElement('div', {className: 'me'}, [title, description, list]);
+
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
+
